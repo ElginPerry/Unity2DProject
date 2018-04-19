f9e4290e31caaeb573bc9e7bef57d951ae7c1c46
ElginPerry <elginPerry@yahoo.com>
2018-04-19 15:03:12 -0400

    Click Movement

---
+++ Assets/Scripts/Player/ClickMovement.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+
+public class ClickMovement : MonoBehaviour
+{
+    /// <summary>
+    /// 1 - The speed of the ship
+    /// </summary>
+
+    public GameObject Player;
+    public float speed = 20f;
+    private Vector3 target;
+
+    private Vector2 movement;
+    private bool ClickMove = false;
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (!EventSystem.current.IsPointerOverGameObject())
+            {
+                target = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                target.z = Player.transform.position.z;
+                ClickMove = true;
+            }
+            else
+            {
+                ClickMove = false;
+            }
+        }
+        
+    }
+
+    void FixedUpdate()
+    {
+        if (ClickMove)
+        {
+            Player.transform.position = Vector3.MoveTowards(Player.transform.position, target, speed * Time.deltaTime);
+        }
+    }
+}
